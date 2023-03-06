34347a5767a10b67e371cc6d2a82af65747c2153
Salim Salim <salimademm@gmail.com>
2023-03-06 21:07:06 +0200

    add navbar design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
-export default function App() {
-  return <div>Hello World</div>;
-}
+import {
+  ChannelDetail,
+  VideoDetail,
+  SearchFeed,
+  Navbar,
+  Feed,
+} from "./components";
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import SearchBar from "./SearchBar";
+
+export default function Navbar() {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        background: "#000",
+        top: "0",
+        justifyContent: "space-between",
+      }}
+    >
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img src={require("../utils/logo1.png")} alt="logo" height={45} />
+      </Link>
+      <SearchBar></SearchBar>
+    </Stack>
+  );
+}
